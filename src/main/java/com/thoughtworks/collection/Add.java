49618a48ed805a525f35c9d46bb75befabd4460d
huazui <2509155771@qq.com>
2019-07-14 14:53:52 +0800

    Add files via upload
    大约半个小时，因为要学习collect生成数列的方法

--- src/main/java/com/thoughtworks/collection/Add.java
+++ src/main/java/com/thoughtworks/collection/Add.java
@@ -2,11 +2,22 @@ package com.thoughtworks.collection;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 public class Add {
+
     public int getSumOfEvens(int leftBorder, int rightBorder) {
-        throw new NotImplementedException();
+        int max=(leftBorder>rightBorder?leftBorder:rightBorder);
+        int min=(leftBorder<rightBorder?leftBorder:rightBorder);
+        int sum=0;
+        for(int i=min;i<=max;i++){
+            sum+=(i%2==0?i:0);
+        }
+return sum;
+
     }
 
     public int getSumOfOdds(int leftBorder, int rightBorder) {
@@ -14,38 +25,31 @@ public class Add {
     }
 
     public int getSumTripleAndAddTwo(List<Integer> arrayList) {
-        throw new NotImplementedException();
+        return arrayList.stream().collect(Collectors.summingInt(item->(item*3+2)));
+
+
     }
 
     public List<Integer> getTripleOfOddAndAddTwo(List<Integer> arrayList) {
-        throw new NotImplementedException();
+
+        return arrayList.stream().map(item -> {
+            if (item % 2 == 1) {
+                item = item * 3 + 2;
+            }
+            return item;
+        }).collect(Collectors.toList());
     }
 
-    public int getSumOfProcessedOdds(List<Integer> arrayList) {
-        throw new NotImplementedException();
-    }
 
-//    public double getMedianOfEven(List<Integer> arrayList) {
-//        throw new NotImplementedException();
-//    }
 
-    public double getAverageOfEven(List<Integer> arrayList) {
-        throw new NotImplementedException();
-    }
-
-    public boolean isIncludedInEvenIndex(List<Integer> arrayList, Integer specialElment) {
-        throw new NotImplementedException();
-    }
+   public double getAverageOfEven(List<Integer>array){
+        return array.stream().filter(item->item%2==0).collect(Collectors.summarizingDouble(item->item)).getAverage();
+   }
 
     public List<Integer> getUnrepeatedFromEvenIndex(List<Integer> arrayList) {
-        throw new NotImplementedException();
-    }
+        return arrayList.stream().filter(item->item%2==0).collect(Collectors.toSet()).stream().collect(Collectors.toList());
+        }
 
-//    public List<Integer> sortByEvenAndOdd(List<Integer> arrayList) {
-//        throw new NotImplementedException();
-//    }
 
-//    public List<Integer> getProcessedList(List<Integer> arrayList) {
-//        throw new NotImplementedException();
-//    }
+
 }
